3a0183944b83e6e6615c1a8a1d1c75dcd78bd63f
Aarya99rag <144744949+Aarya99rag@users.noreply.github.com>
2024-10-06 23:35:09 +0530

    Update index.js

--- init/index.js
+++ init/index.js
@@ -1,5 +1,5 @@
 const mongoose = require("mongoose");
-const Listing = require("../models/listing.js");    // Moves up one directory level.Use this to navigate to the parent directory of the current file's directory.
+const Listing = require("../models/listing.js");    
 const initData = require("./data.js");
 
 main()
